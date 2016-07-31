30e439daf98ea35671727b6fc196d1e60590c7ec
Theo <theocarr777@gmail.com>
2016-07-31 17:53:21 +1200

    initial commit

---
+++ Standard Assets Example Project/Assets/grabbable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grabbable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print("test");
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+       // print(col);
+        if (col.gameObject.tag == "lefthand")
+        {
+            print("left hand");
+        }
+        if (col.gameObject.tag == "righthand")
+        {
+            print("right hand");
+        }
+    }
+}
