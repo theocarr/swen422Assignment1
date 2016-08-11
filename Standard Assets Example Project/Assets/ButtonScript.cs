27ebf492587578ce796402b96181043b5a1f34dc
Theo <theocarr777@gmail.com>
2016-08-11 22:05:19 +1200

    Add button prefab in assets
    The button has a script with a method called "ButtonClicked" so just put
    code in there

---
+++ Standard Assets Example Project/Assets/ButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+            // print(col);
+            if (col.gameObject.tag == "lefthand")
+            {
+                buttonClicked();
+            }
+            if (col.gameObject.tag == "righthand")
+            {
+                buttonClicked();
+            }
+        }
+
+    void buttonClicked()
+    {
+        print("Button is clicked");
+    }
+}
+
+
